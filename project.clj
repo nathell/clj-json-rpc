2e6469d8b6ff2d256d7be12e0cf8006024529a10
Daniel Janus <dj@danieljanus.pl>
2013-06-20 00:46:38 +0100

    Bump version number and dependencies.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-json-rpc "0.1"
+(defproject clj-json-rpc "0.1.1"
   :description "A JSON-RPC server for Clojure, suitable for use with Ring."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/data.json "0.1.2"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/data.json "0.2.2"]])
